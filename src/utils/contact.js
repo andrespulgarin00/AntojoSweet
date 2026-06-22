18cefef3c20aba14080c2356579449bd75bae7d6
wljuancho <quinteroroldanjuanangel@gmail.com>
2026-06-22 16:02:00 -0500

    m

--- src/utils/contact.js
+++ src/utils/contact.js
@@ -1,4 +1,14 @@
 import footer from "./components/footer.js";
+<<<<<<< HEAD:frontend/src/utils/contact.js
+<<<<<<< HEAD
+footer()
+=======
 import header from "./components/header.js";
 footer();
 header();
+>>>>>>> 33df16e9b938fa6fcc02effe34e2408b73da3491
+=======
+import header from "./components/header.js";
+footer();
+header();
+>>>>>>> ffde281101d4a9edc7945472fa91cfe3d20617e6:src/utils/contact.js
